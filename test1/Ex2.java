87a92c63082ea42ccf5f38cc607e2a65374a5496
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-04-18 15:19:08 +0900

    first commit

---
+++ test1/Ex2.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class Ex2 {
+	public static void main(String[] args) {
+			System.out.println("ohayou");
+			System.out.println("konnitiha");
+			System.out.println("konbanwa");
+	}
+	
+}
