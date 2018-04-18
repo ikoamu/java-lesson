5af972cfbf8357ad3730cf4de58089d7e1e48d58
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-04-18 15:30:47 +0900

    frstcmt

---
+++ test1/Calc1.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class Calc1 {
+	public static void main (String[] args) {
+			System.out.println("2 + 2 = "+(2+2));
+			System.out.println("2 - 2 = "+(2-2));
+			System.out.println("2 * 2 = "+(2*2));
+			System.out.println("2 / 2 = "+(2/2));
+			
+		}	
+	}
