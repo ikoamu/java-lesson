a050f1a7d8dc0087d6dff15d38bfecddc385d2f6
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-04-23 15:19:09 +0900

    コミット修正(2タブに)

--- test2/List_12_9.java
+++ test2/List_12_9.java
@@ -6,34 +6,31 @@
  */
 
 public class List_12_9{
-	
-	public static void main(String[] args) {
-		NamedRectangle nr = new NamedRectangle();
-		nr.goodMethod();
-	}
-
-}
+  public static void main(String[] args) {
+    NamedRectangle nr = new NamedRectangle();
+    nr.goodMethod();
+   }
+ }
 class Rectangle{	
-	
-	private	int	width;
-	private int height;
+  private int width;
+  private int height;
 
-	int getWidth() {
-		return width;
-	}
-	int getHeight() {
-		return height;
-	}
-	void setSize(int width,int height) {
-		this.width = width;
-		this.height = height;
-	}
+  int getWidth() {
+    return width;
+  }
+  int getHeight(){
+    return height;
+  }
+  void setSize(int width,int height) {
+    this.width = width;
+    this.height = height;
+  }
 }
 
 
 class NamedRectangle extends Rectangle{
-	void goodMethod() {
-		setSize(456,78);
-		System.out.println(getHeight());
-	}
+  void goodMethod() {
+    setSize(456,78);
+    System.out.println(getHeight());
+  }
 }
