573f0c3a1ea2fcb3bcdd09a837874cf49e6bbbac
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-04-24 14:00:45 +0900

    p83 List 13-3

---
+++ Q13/Q13_2.java
@@ -0,0 +1,31 @@
+package Q13;
+
+/**
+ * Java言語プログラミングレッスン 下 p100 問題13-3
+ * 
+ * List 13-3 (p83)の例外処理をmainメソッドからmyAssignメソッドに移してください。
+ * すなわち、mainメソッドにはmyAssignメソッドの呼び出しだけ置き、 try文はmyAssignメソッド中におさめてください。
+ *
+ */
+public class Q13_2 {
+  public static void main(String[] args) {
+    int[] myarray = new int[3];
+
+    try {
+      System.out.println("代入します");
+      myAssign(myarray, 100, 0);
+      System.out.println("代入しました");
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("代入できませんでした");
+      System.out.println("例外は" + e + "です");
+    }
+
+    System.out.println("終了します");
+  }
+
+  static void myAssign(int[] arr, int index, int value) {
+    System.out.println("myAssignにきました");
+    arr[index] = value;
+    System.out.println("myAssignから帰ります");
+  }
+}
