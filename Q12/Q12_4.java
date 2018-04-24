78c5bb2374531f9cdd6fdf3e1a600300f9a5c293
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-04-24 11:00:22 +0900

    インデント修正

--- Q12/Q12_4.java
+++ Q12/Q12_4.java
@@ -9,58 +9,59 @@ package Q12;
  * 　1.引数なし　2.位置付き 3.位置と大きさ付き
  * - 位置を変更するメソッドsetLocationを持つ 
  * - 標準的な文字列表現を返すメソッドtoStringを持つ
+ * 
  */
 public class Q12_4 {
-	public static void main(String[] args) {
-		PlaceRectangle a, b, c;
-	  a = new PlaceRectangle();
-		b = new PlaceRectangle(1,2);
-		c = new PlaceRectangle(1,2,3,4);
-		System.out.println("a = " + a);
-		System.out.println("b = " + b);
-		System.out.println("c = " + c);
-	}
+  public static void main(String[] args) {
+    PlaceRectangle a, b, c;
+    a = new PlaceRectangle();
+    b = new PlaceRectangle(1,2);
+    c = new PlaceRectangle(1,2,3,4);
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+    System.out.println("c = " + c);
+  }
 }
 
 class Rectangle{
-	int width;
-	int height;	
-	Rectangle(){ //コンストラクタ(引数なし)
-		setSize(0,0);
-	}
-	Rectangle(int width,int height){ //コンストラクタ(大きさ付き)
-		setSize(width,height);
-	}		
-	void setSize(int width,int height) {
-		this.width = width;
-		this.height = height;
-	}
-	@Override
-	public String toString() {
-		return ("["+width+", "+height+"]");
-	}
+  int width;
+  int height;	
+  Rectangle(){ //コンストラクタ(引数なし)
+    setSize(0,0);
+  }
+  Rectangle(int width,int height){ //コンストラクタ(大きさ付き)
+    setSize(width,height);
+  }		
+  void setSize(int width,int height) {
+    this.width = width;
+    this.height = height;
+  }
+  @Override
+  public String toString() {
+    return ("["+width+", "+height+"]");
+  }
 }
 class PlaceRectangle extends Rectangle{
   int x;
   int y;
   PlaceRectangle(){ //コンストラクタ(引数なし)
-  	setSize(0,0);
-  	setLocation(0,0);
+    setSize(0,0);
+    setLocation(0,0);
   }
   PlaceRectangle(int x,int y){//コンストラクタ(位置付き)
-  	setSize(0,0);
-  	setLocation(x,y);
+    setSize(0,0);
+    setLocation(x,y);
   }
   PlaceRectangle(int x,int y,int width,int height){//コンストラクタ(位置と大きさ付き)
-  	setSize(width,height);
-  	setLocation(x,y);
+    setSize(width,height);
+    setLocation(x,y);
   }
   void setLocation(int x,int y) {
-  	this.x = x;
-  	this.y = y;
+    this.x = x;
+    this.y = y;
   }
   @Override
   public String toString() {
-  	return "[ (" + x + ", " + y + ") ["+width+", "+height+"] ]";
+    return "[ (" + x + ", " + y + ") ["+width+", "+height+"] ]";
   }
 }
