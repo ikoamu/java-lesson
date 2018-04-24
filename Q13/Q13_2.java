a770cb639d88461bcfb32765e34ee437b02db49d
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-04-24 15:38:47 +0900

    例外処理をmyAssignメソッドに移動

--- Q13/Q13_2.java
+++ Q13/Q13_2.java
@@ -10,22 +10,23 @@ package Q13;
 public class Q13_2 {
   public static void main(String[] args) {
     int[] myarray = new int[3];
-
-    try {
-      System.out.println("代入します");
-      myAssign(myarray, 100, 0);
-      System.out.println("代入しました");
-    } catch (ArrayIndexOutOfBoundsException e) {
-      System.out.println("代入できませんでした");
-      System.out.println("例外は" + e + "です");
-    }
-
+    myAssign(myarray, 100, 0);
     System.out.println("終了します");
   }
 
   static void myAssign(int[] arr, int index, int value) {
     System.out.println("myAssignにきました");
-    arr[index] = value;
+
+    try {
+      System.out.println("代入します");
+      arr[index] = value;
+      System.out.println("代入しました");
+
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("代入できませんでした");
+      System.out.println("例外は" + e + "です");
+    }
+
     System.out.println("myAssignから帰ります");
   }
 }
