7278d7964988eeaa121dec0f026a02a931cdc2cf
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-04-25 16:26:54 +0900

    p151 スレッドの簡単な例

---
+++ Q16/CountTenA.java
@@ -0,0 +1,23 @@
+package Q16;
+
+/**
+ * Java言語プログラミングレッスン 下 
+ * p151 List16-1 スレッドの簡単な例
+ */
+
+public class CountTenA extends Thread {
+  public static void main(String[] args) {
+    CountTenA ct = new CountTenA();
+    ct.start();
+    for (int i = 0; i < 10; i++) {
+      System.out.println("main:i = " + i);
+    }
+  }
+
+  @Override
+  public void run() {
+    for (int i = 0; i < 10; i++) {
+      System.out.println("run:i = " + i);
+    }
+  }
+}
