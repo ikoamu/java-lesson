a2a2dbd3c0683c5c579a5835d7c56613d6624806
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-04-26 14:06:58 +0900

    空行を削除

--- Q13/Q13_2.java
+++ Q13/Q13_2.java
@@ -21,7 +21,6 @@ public class Q13_2 {
       System.out.println("代入します");
       arr[index] = value;
       System.out.println("代入しました");
-
     } catch (ArrayIndexOutOfBoundsException e) {
       System.out.println("代入できませんでした");
       System.out.println("例外は" + e + "です");
